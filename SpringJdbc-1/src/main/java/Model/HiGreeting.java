44c519dd29748fc58c3b51395832917628232bec
Anirban Sengupta <sengupta.ani.80@gmail.com>
2019-10-23 11:32:51 -0700

    Model Classes

---
+++ SpringJdbc-1/src/main/java/Model/HiGreeting.java
@@ -0,0 +1,19 @@
+package Model;
+
+public class HiGreeting {
+	
+	private String content;
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public HiGreeting(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
